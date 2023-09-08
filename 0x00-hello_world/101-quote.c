53a2de4065a28ee0b3a667ba09d635c694ce279a
rahidah <salmahrahyder@gmail.com>
2023-09-07 17:00:47 -0700

    Hello_World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #include <stdio.h>
 #include <unistd.h>
 
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #!/bin/bash
 #include<stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return:always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
